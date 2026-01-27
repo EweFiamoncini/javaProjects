c108f6ab53b6ec94fad7311d38a1a319eb6f1702
Ewerton Fiamoncini <ewertonfiamoncini20@gmail.com>
2026-01-27 07:07:02 -0300

    feat: Simplify ListaCompras by removing user input and adding predefined items

--- Sintaxe Básica/Estruturas de Controle/ListaCompras.java
+++ Sintaxe Básica/Estruturas de Controle/ListaCompras.java
@@ -1,26 +1,22 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ListaCompras {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
         // declarando e inicializando a lista
         ArrayList<String> listaCompras = new ArrayList<>();
 
+        // criando lista de compras
+        listaCompras.add("Café");
+        listaCompras.add("Pão");
+        listaCompras.add("Queijo");
+        listaCompras.add("Presunto");
+        listaCompras.add("Açúcar");
+        listaCompras.add("Leite");
+
         // exibir a lista de compras
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Item " + (i + 1) + " da lista: ");
-            listaCompras.add(scanner.next());
+        for (String lista : listaCompras) {
+            System.out.println(lista);
         }
-
-        System.out.println(listaCompras.size());
-
-        for (String item : listaCompras) {
-            System.out.println("Item cadastrado na posição " + listaCompras.indexOf(item) + ": " + item);
-        }
-
-        scanner.close();
     }
 }
