cef536f3aac9ef8b366a9b9785119ed71de7ba7e
Ewerton Fiamoncini <ewertonfiamoncini20@gmail.com>
2026-01-27 06:58:46 -0300

    feat: Update ListaCompras to allow user input for shopping list items

--- Sintaxe Básica/Estruturas de Controle/ListaCompras.java
+++ Sintaxe Básica/Estruturas de Controle/ListaCompras.java
@@ -1,19 +1,26 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListaCompras {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // declarando e inicializando a lista
         ArrayList<String> listaCompras = new ArrayList<>();
 
-        listaCompras.add("Café");
-        listaCompras.add("Cerveja");
-        listaCompras.add("Carne");
-
-        listaCompras.remove(1);
+        // exibir a lista de compras
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Item " + (i + 1) + " da lista: ");
+            listaCompras.add(scanner.next());
+        }
 
         System.out.println(listaCompras.size());
 
         for (String item : listaCompras) {
-            System.out.println("Item: " + item);
+            System.out.println("Item cadastrado na posição " + listaCompras.indexOf(item) + ": " + item);
         }
+
+        scanner.close();
     }
 }
