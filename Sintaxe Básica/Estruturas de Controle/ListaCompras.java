eb70896a1d0a0a7045477949c637f14d71ed81eb
Ewerton Fiamoncini <ewertonfiamoncini20@gmail.com>
2026-01-27 07:13:38 -0300

    feat: Improve output formatting in ListaCompras for better readability

--- Sintaxe Básica/Estruturas de Controle/ListaCompras.java
+++ Sintaxe Básica/Estruturas de Controle/ListaCompras.java
@@ -14,13 +14,28 @@ public class ListaCompras {
         listaCompras.add("Açúcar");
         listaCompras.add("Leite");
 
-        //exibindo tamanho da lista
+        // Titulo da lista
+        System.out.println("---- LISTA DE COMPRAS ----");
+
+        // exibindo tamanho da lista
+        System.out.println("");
         System.out.println("Número de itens na lista de compras: " + listaCompras.size());
 
+        // exibindo primeiro item da lista
+        System.out.println("");
+        System.out.println("Primeiro item da lista: " + listaCompras.get(0));
+
+        // exibindo ultimo item da lista
+        System.out.println("");
+        System.out.println("Ultimo item da lista: " + listaCompras.get(listaCompras.size() - 1));
+
         // exibir a lista de compras
+        System.out.println("");
         System.out.println("Lista de Compras:");
         for (String lista : listaCompras) {
             System.out.println(lista);
         }
+
+        System.out.println("");
     }
 }
