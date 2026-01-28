baeb0231ce575bc351bd22e922545f53cfdcd3c5
Ewerton Fiamoncini <ewertonfiamoncini20@gmail.com>
2026-01-28 13:35:35 -0300

    feat: Enhance SwtichCase class with additional output for unrecognized input and program exit

--- Sintaxe Básica/Estruturas de Controle/SwtichCase.java
+++ Sintaxe Básica/Estruturas de Controle/SwtichCase.java
@@ -11,32 +11,41 @@ public class SwtichCase {
         // declarando variaveis
         String palavra = "";
 
+        System.out.println("=== Estrutura Switch-Case ===");
+        System.out.println("");
+
         System.out.print("Digite uma palavra (casa, carro, moto, aviao): ");
         palavra = scanner.nextLine(); // lendo a palavra digitada pelo usuario
 
         // estrutura switch-case para verificar o valor da variavel 'palavra'
         switch (palavra.toLowerCase()) { // convertendo a palavra para minusculo para evitar erros de comparacao
             case "casa":
+                System.out.println("");
                 System.out.println("Palavra encontrada!");
                 System.out.println("");
                 break; // encerrando o case
             case "carro":
+                System.out.println("");
                 System.out.println("Palavra encontrada!");
                 System.out.println("");
                 break; // encerrando o case
             case "moto":
+                System.out.println("");
                 System.out.println("Palavra encontrada!");
                 System.out.println("");
                 break; // encerrando o case
             case "aviao":
+                System.out.println("");
                 System.out.println("Palavra encontrada!");
                 System.out.println("");
                 break; // encerrando o case
             case "sair":
+                System.out.println("");
                 System.out.println("Encerrando o programa. Ate mais!");
                 System.out.println("");
                 break; // encerrando o case sair
             default:
+                System.out.println("");
                 System.out.println("Palavra nao reconhecida.");
                 System.out.println("");
                 break; // encerrando o case padrao
